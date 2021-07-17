dd4ba6f0d31f236bba63ebe5274c0bf9fb072b9a
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-17 19:27:28 +0000

    Initial Commit

---
+++ COntextMenuDemo/COntextMenuDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  COntextMenuDemoApp.swift
+//  COntextMenuDemo
+//
+//  Created by Azu on 17/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct COntextMenuDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
