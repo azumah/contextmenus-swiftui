4fa654f4877a1d9841ef80e5d3db4e49ce906c97
Nelson Klutse <1698197+azumah@users.noreply.github.com>
2021-07-17 23:13:30 +0000

    add context menu

--- COntextMenuDemo/ContentView.swift
+++ COntextMenuDemo/ContentView.swift
@@ -8,9 +8,31 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var foregroundColor: Color = Color.black
+    @State private var backgroundColor: Color = Color.white
+    
     var body: some View {
-        Text("Hello, world!")
+        Text("Long Press Me!!")
             .padding()
+            .font(.largeTitle)
+            .foregroundColor(foregroundColor)
+            .background(backgroundColor)
+            .contextMenu{
+                Button(action: {
+                    self.foregroundColor = .black
+                    self.backgroundColor = .white
+                }, label: {
+                    Text("Normal Colors")
+                    Image(systemName: "paintbrush")
+                })
+                Button(action: {
+                    self.foregroundColor = .white
+                    self.backgroundColor = .black
+                }, label: {
+                    Text("Inverted Colors")
+                    Image(systemName: "paintbrush.fill")
+                })
+            }
     }
 }
 
